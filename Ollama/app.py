abfa0cc9c9fed164b4f15d9c9f1de29278d25268
Devendra Kashyap <dkashyap099@gmail.com>
2026-09-15 01:32:44 +0530

    Change input handling to use a submit button

--- Ollama/app.py
+++ Ollama/app.py
@@ -33,5 +33,5 @@ llm = Ollama(model="gemma:2b")
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 
-if input_text:
+if st.button('Submit'):
     st.write((chain.invoke({'question':input_text})))
